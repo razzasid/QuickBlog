4abfb61692c36b795e07206f26608c11a4035281
Raza Siddique <razamohd6395@gmail.com>
2025-08-08 17:16:04 +0000

    add bloglist

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { comments_data } from "../../assets/assets";
+import CommentTableItem from "../../components/admin/CommentTableItem";
 
 const Comments = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [comments, setComments] = useState([]);
+  const [filter, setFilter] = useState("Not Approved");
 
-export default Comments
+  const fetchComments = async () => {
+    setComments(comments_data);
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  return (
+    <div className="min-h-screen flex-1 bg-blue-50/50 px-5 pt-5 sm:pt-12 sm:pl-16">
+      <div className="flex max-w-3xl items-center justify-between">
+        <h1>Comments</h1>
+        <div className="flex gap-4">
+          <button
+            onClick={() => setFilter("Approved")}
+            className={`shadow-custom-sm cursor-pointer rounded-full border px-4 py-1 text-xs ${filter === "Approved" ? "text-primary" : "text-gray-700"}`}
+          >
+            Approved
+          </button>
+          <button
+            onClick={() => setFilter("Not Approved")}
+            className={`shadow-custom-sm cursor-pointer rounded-full border px-4 py-1 text-xs ${filter === "Not Approved" ? "text-primary" : "text-gray-700"}`}
+          >
+            Not Approved
+          </button>
+        </div>
+      </div>
+
+      <div className="scrollbar-hide relative mt-4 h-4/5 max-w-3xl overflow-x-auto rounded-lg bg-white shadow">
+        <table className="w-full text-sm text-gray-500">
+          <thead className="text-left text-xs text-gray-700 uppercase">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Blog Title & Comment
+              </th>
+              <th scope="col" className="px-6 py-3 max-sm:hidden">
+                Date
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {comments
+              .filter((comment) => {
+                if (filter === "Approved") return comment.isApproved === true;
+                return comment.isApproved === false;
+              })
+              .map((comment, index) => (
+                <CommentTableItem
+                  key={comment._id}
+                  comment={comment}
+                  index={index + 1}
+                  fetchComments={fetchComments}
+                />
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
--- client/src/pages/admin/ListBlog.jsx
+++ client/src/pages/admin/ListBlog.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { blog_data } from "../../assets/assets";
+import BlogTableItem from "../../components/admin/BlogTableItem";
 
 const ListBlog = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [blogs, setBlogs] = useState([]);
 
-export default ListBlog
+  const fetchBlogs = async () => {
+    setBlogs(blog_data);
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
+  return (
+    <div className="flex-1 bg-blue-50/50 px-5 pt-5 sm:pt-12 sm:pl-16">
+      <h1>ALl Blogs</h1>
+
+      <div className="scollbar-hide relative mt-4 h-4/5 max-w-4xl overflow-x-auto rounded-lg bg-white shadow">
+        <table className="w-full text-sm text-gray-500">
+          <thead className="text-left text-xs text-gray-600 uppercase">
+            <tr>
+              <th scope="col" className="px-2 py-4">
+                #
+              </th>
+              <th scope="col" className="px-2 py-4">
+                Blog Title
+              </th>
+              <th scope="col" className="px-2 py-4 max-sm:hidden">
+                Date
+              </th>
+              <th scope="col" className="px-2 py-4 max-sm:hidden">
+                Status
+              </th>
+              <th scope="col" className="px-2 py-4">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {blogs.map((blog, index) => {
+              return (
+                <BlogTableItem
+                  key={blog._id}
+                  blog={blog}
+                  fetchBlogs={fetchBlogs}
+                  index={index + 1}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ListBlog;
